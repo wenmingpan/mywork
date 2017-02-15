9897bf6405fcd2522f48eb2ba8f3224f0a5d2c79
pwm5712 <pwm5712@163.com>
2017-02-15 17:27:43 +0800

    项目部署
    项目部署

---
+++ application/admin/controller/Account.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+use think\Request;
+use think\Db;
+/**
+ * Description of Account
+ *
+ * @author Administrator
+ */
+class Account extends Controller{
+    //put your code here
+    public function index()
+    {
+        return view();
+    }
+    
+}
